52984231a42b07a1f7d5251da39fa2f00dd4dd14
qidongz <qdzhangcn@gmail.com>
2021-01-28 20:47:31 +0800

    Init config

---
+++ init.el
@@ -0,0 +1,7 @@
+(setq inhibit-splash-screen 1)
+
+(setq package-archives '(("gnu"   . "http://mirrors.bfsu.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.bfsu.edu.cn/elpa/melpa/")))
+(package-initialize)
+
+(set-face-attribute 'default nil :height 160)
