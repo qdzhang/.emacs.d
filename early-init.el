29935c84519904f763dccd3fcaf50f39429f0963
qdzhang <qdzhangcn@gmail.com>
2021-05-20 13:42:27 +0800

    Determine whether in GUI emacs

--- early-init.el
+++ early-init.el
@@ -1,10 +1,14 @@
 (setq inhibit-splash-screen 1)
 
-(scroll-bar-mode -1)        ; Disable visible scrollbar
-(tool-bar-mode -1)          ; Disable the toolbar
-(tooltip-mode -1)           ; Disable tooltips
+(and (bound-and-true-p scroll-bar-mode)
+     (scroll-bar-mode -1))        ; Disable visible scrollbar
+(and (bound-and-true-p tool-bar-mode)
+     (tool-bar-mode -1))          ; Disable the toolbar
+(and (bound-and-true-p tooltip-mode)
+     (tooltip-mode -1))           ; Disable tooltips
+(and (bound-and-true-p menu-bar-mode)
+     (menu-bar-mode -1))          ; Disable the menu bar
 (set-fringe-mode 10)        ; Give some breathing room
-(menu-bar-mode -1)          ; Disable the menu bar
 
 ;; Speed up startup
 ;;=================
