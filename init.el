b7996ce349866e48214741bb336212faca03945f
qdzhang <qdzhangcn@gmail.com>
2021-05-22 14:54:45 +0800

    Add config for sly

--- init.el
+++ init.el
@@ -456,3 +456,73 @@
   (c++-mode . eglot-ensure)
   :config
   (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd")))
+
+(use-package sly
+  :defer t
+  :config
+  (setq inferior-lisp-program "/usr/bin/sbcl")
+
+  ;; Define functionality for interacting with the sly repl using counsel
+  (defun counsel-sly-mrepl-shortcut ()
+    (interactive)
+    (ivy-read
+     "Action: "
+     (mapcar #'car sly-mrepl-shortcut-alist)
+     :action (lambda (string)
+               (let ((command (and string
+                                   (cdr (assoc string sly-mrepl-shortcut-alist)))))
+                 (call-interactively command)))))
+
+  (defun sly-read-package-name (prompt &optional initial-value allow-blank)
+    (ivy-read
+     "Package: "
+     (sly-eval `(slynk:list-all-package-names t))
+     :action #'identity))
+
+  (defun counsel-sly-mrepl-history ()
+    (interactive)
+    (ivy-read
+     "History: "
+     (ring-elements comint-input-ring)
+     :action (lambda (e)
+               (insert e))))
+
+  (defun eval-grab-output (string)
+    (let ((res nil))
+      (sly-eval-async `(slynk:eval-and-grab-output ,string)
+        (lambda (result)
+          (cl-destructuring-bind (output value) result
+            (setf res (car (read-from-string value))))))
+      (while (null res)
+        (sleep-for 0.1))
+      res))
+
+  (defun counsel-sly-eval (string action)
+    (let ((result (eval-grab-output string)))
+      (ivy-read
+       "Symbol: "
+       result
+       :action action)))
+
+  (defun send-input (expr)
+    (insert expr)
+    (comint-send-input))
+
+  (defun counsel-sly-package-internal-symbols ()
+    (interactive)
+    (counsel-sly-eval "(common-lisp-user::package-internal-symbols \*package\*)"
+                      `(1 ("o" ,#'insert "insert")
+                          ("f" ,(lambda (candidate)
+                                  (send-input (format "(find-symbol \"%s\")" candidate)))
+                           "find symbol")
+                          )))
+
+  ;; sly-mrepl-mode-map symbol not available at the time of
+  ;; use-package, so :bind cannot be used here
+  ;; (with-eval-after-load 'sly-mrepl
+  ;;   (define-key sly-mrepl-mode-map [remap isearch-backward] 'counsel-sly-mrepl-history)
+  ;;   (define-key sly-mrepl-mode-map (kbd ",,") 'counsel-sly-mrepl-shortcut)
+  ;;   (define-key sly-mrepl-mode-map (kbd ",p") 'sly-mrepl-set-package)
+  ;;   (define-key sly-mrepl-mode-map (kbd ",s") 'counsel-sly-package-internal-symbols))
+  )
+
