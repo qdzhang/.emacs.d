68dc929231cd82d7d3a8a5883027bfe48b68112c
qdzhang <qdzhangcn@gmail.com>
2021-05-25 11:53:38 +0800

    Move evil-mode-line-tag to beginning of modeline

--- init.el
+++ init.el
@@ -327,13 +327,14 @@
   (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
   (setq evil-want-keybinding nil)
   (setq evil-undo-system 'undo-fu)
-  (setq evil-normal-state-tag   (propertize " NORMAL " 'face '((:background "dark khaki" :foreground "black")))
-	evil-emacs-state-tag    (propertize " EMACS " 'face '((:background "turquoise" :foreground "black")))
-	evil-insert-state-tag   (propertize " ------ " 'face '((:background "dark sea green" :foreground "black")))
-	evil-replace-state-tag  (propertize " REPLACE " 'face '((:background "dark orange" :foreground "black")))
-	evil-motion-state-tag   (propertize " MOTION " 'face '((:background "khaki" :foreground "black")))
-	evil-visual-state-tag   (propertize " VISUAL " 'face '((:background "light salmon" :foreground "black")))
-	evil-operator-state-tag (propertize " OPERATE " 'face '((:background "sandy brown" :foreground "black"))))
+  (setq evil-mode-line-format '(before . mode-line-front-space)) ; Move evil-mode-line-tag to beginning of modeline
+  (setq evil-normal-state-tag   (propertize " NORMAL " 'face '((:foreground "dark khaki")))
+	evil-emacs-state-tag    (propertize " EMACS " 'face '((:foreground "turquoise")))
+	evil-insert-state-tag   (propertize " INSERT " 'face '((:foreground "dark sea green")))
+	evil-replace-state-tag  (propertize " REPLACE " 'face '((:foreground "dark orange")))
+	evil-motion-state-tag   (propertize " MOTION " 'face '((:foreground "khaki")))
+	evil-visual-state-tag   (propertize " VISUAL " 'face '((:foreground "light salmon")))
+	evil-operator-state-tag (propertize " OPERATE " 'face '((:foreground "sandy brown"))))
   :config
   (evil-mode 1))
 
