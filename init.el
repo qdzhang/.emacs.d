8b1b06c78a634cfa82701be1cd5e0b6f32008260
qdzhang <qdzhangcn@gmail.com>
2021-06-15 17:22:37 +0800

    Copy spacemacs core-display-init to set faces

--- init.el
+++ init.el
@@ -1,10 +1,53 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
-(set-face-attribute
- 'default nil
- :font (font-spec :family "Sarasa Mono SC" :size 24))
-(set-fontset-font t 'han "LXGW WenKai")
-(set-fontset-font t 'symbol "Noto Color Emoji" nil 'append)
+
+;; Setting after display system init list
+;; If you start an emacsclient, this macro is helpful to set font faces
+;; or some other things should be set after frame created
+;; Copied from spacemacs
+;; https://github.com/syl20bnr/spacemacs/blob/ca04186cfcde09f5b03951353cedd4f828c69378/core/core-display-init.el
+(defvar spacemacs--after-display-system-init-list '()
+  "List of functions to be run after the display system is initialized.")
+
+(defadvice server-create-window-system-frame
+    (after spacemacs-init-display activate)
+  "After Emacs server creates a frame, run functions queued in
+`SPACEMACS--AFTER-DISPLAY-SYSTEM-INIT-LIST' to do any setup that needs to have
+the display system initialized."
+  (progn
+    (dolist (fn (reverse spacemacs--after-display-system-init-list))
+      (funcall fn))
+    (ad-disable-advice 'server-create-window-system-frame
+                       'after
+                       'spacemacs-init-display)
+    (ad-activate 'server-create-window-system-frame)))
+
+(defmacro spacemacs|do-after-display-system-init (&rest body)
+  "If the display-system is initialized, run `BODY', otherwise,
+add it to a queue of actions to perform after the first graphical frame is
+created."
+  `(let ((init (cond ((boundp 'ns-initialized) ns-initialized)
+                     ;; w32-initialized gets set too early, so
+                     ;; if we're on Windows, check the list of fonts
+                     ;; instead (this is nil until the graphics system
+                     ;; is initialized)
+                     ((boundp 'w32-initialized) (font-family-list))
+                     ((boundp 'x-initialized) x-initialized)
+                     ;; fallback to normal loading behavior only if in a GUI
+                     (t (display-graphic-p)))))
+     (if init
+         (progn
+           ,@body)
+       (push (lambda () ,@body) spacemacs--after-display-system-init-list))))
+
+(spacemacs|do-after-display-system-init
+ (set-face-attribute
+  'default
+  nil
+  :font (font-spec :family "Sarasa Mono SC" :size 24))
+ (set-fontset-font t 'han "LXGW WenKai")
+ (set-fontset-font t 'symbol "Noto Color Emoji" nil 'append))
+
 
 (require 'package)
 
