486fb96a99b03aa35683cec1cfab7b079a2ff688
qdzhang <qdzhangcn@gmail.com>
2021-06-15 10:54:45 +0800

    Add a function to git clone clipboard url

--- init.el
+++ init.el
@@ -646,9 +646,50 @@ Ignores `ARGS'."
   ;; Make magit status show full screen
   ;; https://github.com/magit/magit/issues/1953#issuecomment-221134023
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
+
+
+  ;; git clone from clipboard
+  ;; https://xenodium.com/emacs-clone-git-repo-from-clipboard/
+  (defun my/git-clone-clipboard-url ()
+    "Clone git URL in clipboard asynchronously and open in dired when finished."
+    (interactive)
+    (cl-assert (string-match-p "^\\(http://\\|https://\\|ssh://\\|git@\\)" (current-kill 0)) nil "No URL in clipboard")
+    (let* ((url (current-kill 0))
+           (download-dir (expand-file-name default-directory))
+           (project-dir (concat (file-name-as-directory download-dir)
+				(file-name-base url)))
+           (default-directory download-dir)
+           (command (format "git clone %s" url))
+           (buffer (generate-new-buffer (format "*%s*" command)))
+           (proc))
+      (when (file-exists-p project-dir)
+	(if (y-or-n-p (format "%s exists. delete?" (file-name-base url)))
+            (delete-directory project-dir t)
+          (user-error "Bailed")))
+      (switch-to-buffer buffer)
+      (setq proc (start-process-shell-command (nth 0 (split-string command)) buffer command))
+      (with-current-buffer buffer
+	(setq default-directory download-dir)
+	(shell-command-save-pos-or-erase)
+	(require 'shell)
+	(shell-mode)
+	(view-mode +1))
+      (set-process-sentinel proc (lambda (process state)
+                                   (let ((output (with-current-buffer (process-buffer process)
+                                                   (buffer-string))))
+                                     (kill-buffer (process-buffer process))
+                                     (if (= (process-exit-status process) 0)
+					 (progn
+                                           (message "finished: %s" command)
+                                           (dired project-dir))
+                                       (user-error (format "%s\n%s" command output))))))
+      (set-process-filter proc #'comint-output-filter)))
+
+
   :general
   (my/leader-keys
-    "gl" 'magit-log-buffer-file))
+    "gl" 'magit-log-buffer-file
+    "gc" '(my/git-clone-clipboard-url :wk "clone-clipboard")))
 
 (use-package undo-fu
   :config
