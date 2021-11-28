d6b4f63a45410bdf3ed6f66e634acb3fa8df550f
qdzhang <qdzhangcn@gmail.com>
2021-11-28 15:42:05 +0800

    Replace origami with hideshow

--- init.el
+++ init.el
@@ -917,31 +917,6 @@ Start `ielm' in a split window if it's not already running."
 
 (require 'tmtxt-async-tasks)
 
-(require 'hl-todo)
-(global-hl-todo-mode 1)
-(eval-after-load 'which-key (which-key-add-key-based-replacements "C-c t" "hl-todo"))
-(define-key hl-todo-mode-map (kbd "C-c t p") 'hl-todo-previous)
-(define-key hl-todo-mode-map (kbd "C-c t n") 'hl-todo-next)
-(define-key hl-todo-mode-map (kbd "C-c t o") 'hl-todo-occur)
-(define-key hl-todo-mode-map (kbd "C-c t i") 'hl-todo-insert)
-
-
-;; Origami is used as evil folding backend
-
-;; Alternative:
-;; - `hs-minor-mode', can be used in conjunction with [hideshowvis](https://www.emacswiki.org/emacs/hideshowvis.el)
-;; - `outline-mode', can be used in conjunction with [backline](https://github.com/tarsius/backline)
-;;                   and some config examples: https://www.reddit.com/r/emacs/comments/e2u5n9/code_folding_with_outlineminormode/
-
-;; Depends on fringer-helper
-;; Maybe should load after evil
-(require 'origami)
-(global-origami-mode 1)
-;; Some config can be used for reference
-;; TODO: adapt some config examples
-;; https://www.reddit.com/r/emacs/comments/6fmpwb/evil_and_builtin_folding/
-;; https://www.reddit.com/r/emacs/comments/5ei7wa/awesome_vimlike_folding_for_evilmode_with_markers/
-
 (require 'idle-highlight-mode)
 ;; TODO see highlight-overlay
 
@@ -3672,6 +3647,55 @@ Version 2016-08-09"
   :general
   (my/leader-keys
     "or" 'quickrun))
+
+;; Package `hl-todo'
+(require 'hl-todo)
+(global-hl-todo-mode 1)
+(eval-after-load 'which-key (which-key-add-key-based-replacements "C-c t" "hl-todo"))
+(define-key hl-todo-mode-map (kbd "C-c t p") 'hl-todo-previous)
+(define-key hl-todo-mode-map (kbd "C-c t n") 'hl-todo-next)
+(define-key hl-todo-mode-map (kbd "C-c t o") 'hl-todo-occur)
+(define-key hl-todo-mode-map (kbd "C-c t i") 'hl-todo-insert)
+
+;; Origami is used as evil folding backend
+
+;; Alternative:
+;; - `hs-minor-mode', can be used in conjunction with [hideshowvis](https://www.emacswiki.org/emacs/hideshowvis.el)
+;; - `outline-mode', can be used in conjunction with [backline](https://github.com/tarsius/backline)
+;;                   and some config examples: https://www.reddit.com/r/emacs/comments/e2u5n9/code_folding_with_outlineminormode/
+
+;; Depends on fringer-helper
+;; Maybe should load after evil
+;; (use-package origami
+;;   :after evil
+;;   :ensure nil
+;;   :load-path "~/.emacs.d/site-lisp/origami.el/"
+;;   :config
+;;   (global-origami-mode 1))
+
+;; Some config can be used for reference
+;; TODO: adapt some config examples
+;; https://www.reddit.com/r/emacs/comments/6fmpwb/evil_and_builtin_folding/
+;; https://www.reddit.com/r/emacs/comments/5ei7wa/awesome_vimlike_folding_for_evilmode_with_markers/
+
+;; Use hideshow as evil folding backend
+(use-package hideshow
+  :after evil
+  :defer t
+  :ensure nil
+  :diminish hs-minor-mode
+  :hook (prog-mode . hs-minor-mode)
+  :config
+  ;; Make hideshow overlay more conspicuous
+  (defconst hideshow-folded-face '((t (:inherit 'font-lock-comment-face :box t))))
+
+  (defun hideshow-folded-overlay-fn (ov)
+    (when (eq 'code (overlay-get ov 'hs))
+      (let* ((nlines (count-lines (overlay-start ov) (overlay-end ov)))
+             (info (format " ... #%d " nlines)))
+        (overlay-put ov 'display (propertize info 'face hideshow-folded-face)))))
+
+  (setq hs-set-up-overlay 'hideshow-folded-overlay-fn))
 
 
 ;;; Restore file-name-hander-alist
