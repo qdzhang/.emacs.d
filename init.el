5103b4ea321d45ab84df22257e1c784cbb57fbab
qdzhang <qdzhangcn@gmail.com>
2021-12-04 16:59:03 +0800

    Use vanilla org src blocks' style

--- init.el
+++ init.el
@@ -2707,78 +2707,78 @@ respectively."
 
 ;;; Change org-mode src blocks display style, make them more simple and elegant
 ;;; https://emacs-china.org/t/org-source-code/9762/5
-(with-eval-after-load 'org
-  (defvar-local my/org-at-src-begin -1
-    "Variable that holds whether last position was a ")
+;; (with-eval-after-load 'org
+;;   (defvar-local my/org-at-src-begin -1
+;;     "Variable that holds whether last position was a ")
 
-  (defvar my/ob-header-symbol ?…
-    "Symbol used for babel headers")
+;;   (defvar my/ob-header-symbol ?…
+;;     "Symbol used for babel headers")
 
-  (defun my/org-prettify-src--update ()
-    (let ((case-fold-search t)
-          (re "^[ \t]*#\\+begin_src[ \t]+[^ \f\t\n\r\v]+[ \t]*")
-          found)
-      (save-excursion
-        (goto-char (point-min))
-        (while (re-search-forward re nil t)
-          (goto-char (match-end 0))
-          (let ((args (org-trim
-                       (buffer-substring-no-properties (point)
-                                                       (line-end-position)))))
-            (when (org-string-nw-p args)
-              (let ((new-cell (cons args my/ob-header-symbol)))
-                (cl-pushnew new-cell prettify-symbols-alist :test #'equal)
-                (cl-pushnew new-cell found :test #'equal)))))
-        (setq prettify-symbols-alist
-              (cl-set-difference prettify-symbols-alist
-                                 (cl-set-difference
-                                  (cl-remove-if-not
-                                   (lambda (elm)
-                                     (eq (cdr elm) my/ob-header-symbol))
-                                   prettify-symbols-alist)
-                                  found :test #'equal)))
-        ;; Clean up old font-lock-keywords.
-        (font-lock-remove-keywords nil prettify-symbols--keywords)
-        (setq prettify-symbols--keywords (prettify-symbols--make-keywords))
-        (font-lock-add-keywords nil prettify-symbols--keywords)
-        (while (re-search-forward re nil t)
-          (font-lock-flush (line-beginning-position) (line-end-position))))))
+;;   (defun my/org-prettify-src--update ()
+;;     (let ((case-fold-search t)
+;;           (re "^[ \t]*#\\+begin_src[ \t]+[^ \f\t\n\r\v]+[ \t]*")
+;;           found)
+;;       (save-excursion
+;;         (goto-char (point-min))
+;;         (while (re-search-forward re nil t)
+;;           (goto-char (match-end 0))
+;;           (let ((args (org-trim
+;;                        (buffer-substring-no-properties (point)
+;;                                                        (line-end-position)))))
+;;             (when (org-string-nw-p args)
+;;               (let ((new-cell (cons args my/ob-header-symbol)))
+;;                 (cl-pushnew new-cell prettify-symbols-alist :test #'equal)
+;;                 (cl-pushnew new-cell found :test #'equal)))))
+;;         (setq prettify-symbols-alist
+;;               (cl-set-difference prettify-symbols-alist
+;;                                  (cl-set-difference
+;;                                   (cl-remove-if-not
+;;                                    (lambda (elm)
+;;                                      (eq (cdr elm) my/ob-header-symbol))
+;;                                    prettify-symbols-alist)
+;;                                   found :test #'equal)))
+;;         ;; Clean up old font-lock-keywords.
+;;         (font-lock-remove-keywords nil prettify-symbols--keywords)
+;;         (setq prettify-symbols--keywords (prettify-symbols--make-keywords))
+;;         (font-lock-add-keywords nil prettify-symbols--keywords)
+;;         (while (re-search-forward re nil t)
+;;           (font-lock-flush (line-beginning-position) (line-end-position))))))
 
-  (defun my/org-prettify-src ()
-    "Hide src options via `prettify-symbols-mode'.
+;;   (defun my/org-prettify-src ()
+;;     "Hide src options via `prettify-symbols-mode'.
 
-  `prettify-symbols-mode' is used because it has uncollpasing. It's
-  may not be efficient."
-    (let* ((case-fold-search t)
-           (at-src-block (save-excursion
-                           (beginning-of-line)
-                           (looking-at "^[ \t]*#\\+begin_src[ \t]+[^ \f\t\n\r\v]+[ \t]*"))))
-      ;; Test if we moved out of a block.
-      (when (or (and my/org-at-src-begin
-                     (not at-src-block))
-                ;; File was just opened.
-                (eq my/org-at-src-begin -1))
-        (my/org-prettify-src--update))
-      ;; Remove composition if at line; doesn't work properly.
-      ;; (when at-src-block
-      ;;   (with-silent-modifications
-      ;;     (remove-text-properties (match-end 0)
-      ;;                             (1+ (line-end-position))
-      ;;                             '(composition))))
-      (setq my/org-at-src-begin at-src-block)))
+;;   `prettify-symbols-mode' is used because it has uncollpasing. It's
+;;   may not be efficient."
+;;     (let* ((case-fold-search t)
+;;            (at-src-block (save-excursion
+;;                            (beginning-of-line)
+;;                            (looking-at "^[ \t]*#\\+begin_src[ \t]+[^ \f\t\n\r\v]+[ \t]*"))))
+;;       ;; Test if we moved out of a block.
+;;       (when (or (and my/org-at-src-begin
+;;                      (not at-src-block))
+;;                 ;; File was just opened.
+;;                 (eq my/org-at-src-begin -1))
+;;         (my/org-prettify-src--update))
+;;       ;; Remove composition if at line; doesn't work properly.
+;;       ;; (when at-src-block
+;;       ;;   (with-silent-modifications
+;;       ;;     (remove-text-properties (match-end 0)
+;;       ;;                             (1+ (line-end-position))
+;;       ;;                             '(composition))))
+;;       (setq my/org-at-src-begin at-src-block)))
 
-  (defun my/org-prettify-symbols ()
-    (mapc (apply-partially 'add-to-list 'prettify-symbols-alist)
-          (cl-reduce 'append
-                     (mapcar (lambda (x) (list x (cons (upcase (car x)) (cdr x))))
-                             `(("#+begin_src" . ?＄) ; ➤ ➟ ➤ ✎  〜
-                               ("#+end_src"   . ?□) ; ⏹
-                               ("#+header:" . ,my/ob-header-symbol)
-                               ("#+begin_quote" . ?❝) ; » «
-                               ("#+end_quote" . ?❞)))))
-    (turn-on-prettify-symbols-mode)
-    (add-hook 'post-command-hook 'my/org-prettify-src t t))
-  (add-hook 'org-mode-hook #'my/org-prettify-symbols))
+;;   (defun my/org-prettify-symbols ()
+;;     (mapc (apply-partially 'add-to-list 'prettify-symbols-alist)
+;;           (cl-reduce 'append
+;;                      (mapcar (lambda (x) (list x (cons (upcase (car x)) (cdr x))))
+;;                              `(("#+begin_src" . ?＄) ; ➤ ➟ ➤ ✎  〜
+;;                                ("#+end_src"   . ?□) ; ⏹
+;;                                ("#+header:" . ,my/ob-header-symbol)
+;;                                ("#+begin_quote" . ?❝) ; » «
+;;                                ("#+end_quote" . ?❞)))))
+;;     (turn-on-prettify-symbols-mode)
+;;     (add-hook 'post-command-hook 'my/org-prettify-src t t))
+;;   (add-hook 'org-mode-hook #'my/org-prettify-symbols))
 
 (use-package org-journal
   :defer t
