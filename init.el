206448ad1876f9779d7c81565577ca5103e88158
qdzhang <qdzhangcn@gmail.com>
2021-12-16 17:05:27 +0800

    Config built-in show-paren-mode
    - Disable show-smartparens-mode and use built-in show-paren-mode.
    - Config evil not hightlight closing paren and can move beyond
      eol, and this behavior is like vanilla emacs.
    - Show matching lines when parentheses go offscreen.

--- init.el
+++ init.el
@@ -1362,6 +1362,8 @@ buffer is not visiting a file."
   (setq evil-undo-system 'undo-fu)
   (setq evil-respect-visual-line-mode t)
   (setq isearch-lazy-count t)
+  (setq evil-move-beyond-eol t)
+  (setq evil-highlight-closing-paren-at-point-states nil)
   :config
   ;; Move evil-mode-line-tag to beginning of modeline
   ;; If mode-line-format is not configured explicitly, following line can move
@@ -2557,7 +2559,9 @@ become defined after invocation."
 (use-package smartparens-config
   :ensure smartparens
   :diminish smartparens-mode
-  :config (show-smartparens-global-mode t))
+  :config
+  (setq show-smartparens-global-mode nil)
+  (setq show-smartparens-mode nil))
 
 (use-package smartparens
   :hook
@@ -2611,6 +2615,69 @@ respectively."
   :bind
   (:map  evil-cleverparens-mode-map
          ("<normal-state> s" . nil)))
+
+(use-package paren
+  :ensure nil
+  :config
+  ;; Show matched parens overlay offscreen
+  ;; https://with-emacs.com/posts/ui-hacks/show-matching-lines-when-parentheses-go-off-screen/
+  ;; we will call `blink-matching-open` ourselves...
+  (remove-hook 'post-self-insert-hook
+               #'blink-paren-post-self-insert-function)
+  ;; this still needs to be set for `blink-matching-open` to work
+  (setq blink-matching-paren 'show)
+
+  (let ((ov nil))                       ; keep track of the overlay
+    (advice-add
+     #'show-paren-function
+     :after
+     (defun show-paren--off-screen+ (&rest _args)
+       "Display matching line for off-screen paren."
+       (when (overlayp ov)
+         (delete-overlay ov))
+       ;; check if it's appropriate to show match info,
+       ;; see `blink-paren-post-self-insert-function'
+       (when (and (overlay-buffer show-paren--overlay)
+                  (not (or cursor-in-echo-area
+                           executing-kbd-macro
+                           noninteractive
+                           (minibufferp)
+                           this-command))
+                  (and (not (bobp))
+                       (memq (char-syntax (char-before)) '(?\) ?\$)))
+                  (= 1 (logand 1 (- (point)
+                                    (save-excursion
+                                      (forward-char -1)
+                                      (skip-syntax-backward "/\\")
+                                      (point))))))
+         ;; rebind `minibuffer-message' called by
+         ;; `blink-matching-open' to handle the overlay display
+         (cl-letf (((symbol-function #'minibuffer-message)
+                    (lambda (msg &rest args)
+                      (let ((msg (apply #'format-message msg args)))
+                        (setq ov (display-line-overlay+
+                                  (window-start) msg ))))))
+           (blink-matching-open))))))
+
+  (defun display-line-overlay+ (pos str &optional face)
+    "Display line at POS as STR with FACE.
+
+FACE defaults to inheriting from default and highlight."
+    (let ((ol (save-excursion
+                (goto-char pos)
+                (make-overlay (line-beginning-position)
+                              (line-end-position)))))
+      (overlay-put ol 'display str)
+      (overlay-put ol 'face
+                   (or face '(:inherit default :inherit highlight)))
+      ol))
+
+  (setq show-paren-style 'paren
+        show-paren-delay 0.03
+        show-paren-highlight-openparen t
+        show-paren-when-point-inside-paren nil
+        show-paren-when-point-in-periphery t)
+  (show-paren-mode 1))
 
 (use-package terminal-here
   :bind
