103b1e6dbc9c5a5f941df0c6bddc2055fb0f6cba
qdzhang <qdzhangcn@gmail.com>
2022-01-04 15:49:01 +0800

    Minor changes: clear and format some code

--- init.el
+++ init.el
@@ -457,7 +457,6 @@ mouse-1: Display minor modes menu"
            " ")
    'face 'simple-modeline-main-mode))
 
-(buffer-local-value 'major-mode (other-buffer))
 (defun simple-modeline-segment-modified ()
   "Display different icons according to the buffer status"
   (cond
@@ -3503,20 +3502,20 @@ CALLBACK is the function that we need to call when we are done, on all the error
   (defun +lsp--flycheck-eglot--on-diagnostics (diags &rest _)
     (cl-labels
         ((flymake-diag->flycheck-err
-           (diag)
-           (with-current-buffer (flymake--diag-buffer diag)
-             (flycheck-error-new-at-pos
-              (flymake--diag-beg diag)
-              (pcase (flymake--diag-type diag)
-                ('eglot-note 'info)
-                ('eglot-warning 'warning)
-                ('eglot-error 'error)
-                (_ (error "Unknown diagnostic type, %S" diag)))
-              (flymake--diag-text diag)
-              :end-pos (flymake--diag-end diag)
-              :checker 'eglot
-              :buffer (current-buffer)
-              :filename (buffer-file-name)))))
+          (diag)
+          (with-current-buffer (flymake--diag-buffer diag)
+            (flycheck-error-new-at-pos
+             (flymake--diag-beg diag)
+             (pcase (flymake--diag-type diag)
+               ('eglot-note 'info)
+               ('eglot-warning 'warning)
+               ('eglot-error 'error)
+               (_ (error "Unknown diagnostic type, %S" diag)))
+             (flymake--diag-text diag)
+             :end-pos (flymake--diag-end diag)
+             :checker 'eglot
+             :buffer (current-buffer)
+             :filename (buffer-file-name)))))
       (setq +lsp--flycheck-eglot--current-errors
             (mapcar #'flymake-diag->flycheck-err diags))
       ;; Call Flycheck to update the diagnostics annotations
